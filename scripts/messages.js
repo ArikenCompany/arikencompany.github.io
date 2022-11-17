e1cca298262d4126ed0824e1ec360bde53471451
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2022-11-17 23:35:38 +0900

    2.0.0

--- scripts/messages.js
+++ scripts/messages.js
@@ -1,4 +1,4 @@
-fetch("http://suzuki-dev.com:3150/messages")
+fetch("https://suzuki-dev.com:3150/messages")
     .then((response) => response.json())
     .then((ranking) => {
         const array = Object.entries(ranking);
